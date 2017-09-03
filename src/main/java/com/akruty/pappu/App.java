8aac3746e35e6ccbb86dab0f0ad966b9c9c79d86
vishpat <vishpat@gmail.com>
2017-09-03 18:14:26 -0400

    Initial commit for Pappi

---
+++ src/main/java/com/akruty/pappu/App.java
@@ -0,0 +1,102 @@
+package com.akruty.pappu;
+
+import java.util.List;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.protobuf.ByteString;
+
+public class App extends Application
+{
+    private double xOffset;
+    private double yOffset;
+    private VoiceRecoderService voiceRecoderService;
+
+    public static void main( String[] args )
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        voiceRecoderService = new VoiceRecoderService();
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        Button btn = new Button();
+        btn.setText("Oye Pappu");
+
+        btn.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+            voiceRecoderService.restart();
+        });
+
+        btn.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
+        });
+
+        btn.setOnMouseReleased( event -> {
+            SpeechClient speech = null;
+            voiceRecoderService.cancel();
+            byte[] voiceData = voiceRecoderService.getVoiceData();
+
+            try {
+                speech = SpeechClient.create();
+            } catch (java.io.IOException ex) {
+                System.out.println("Unable to create Google speech client, " +
+                        "make sure you have set the environment " +
+                        "variable GOOGLE_APPLICATION_CREDENTIALS " + ex.getMessage());
+                System.exit(1);
+            }
+
+            ByteString audioBytes = ByteString.copyFrom(voiceData);
+
+            System.out.println("Audio byte size " + audioBytes.size());
+
+            // Builds the sync recognize request
+            RecognitionConfig config = RecognitionConfig.newBuilder()
+                    .setEncoding(AudioEncoding.LINEAR16)
+                    .setSampleRateHertz(16000)
+                    .setLanguageCode("en-US")
+                    .build();
+            RecognitionAudio audio = RecognitionAudio.newBuilder()
+                    .setContent(audioBytes)
+                    .build();
+
+            // Performs speech recognition on the audio file
+            RecognizeResponse response = speech.recognize(config, audio);
+            List<SpeechRecognitionResult> results = response.getResultsList();
+
+            System.out.println("Size of results " + results.size());
+
+            for (SpeechRecognitionResult result: results) {
+                // There can be several alternative transcripts for a given chunk of speech. Just use the
+                // first (most likely) one here.
+                SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+                System.out.printf("Transcription: %s%n", alternative.getTranscript());
+            }
+
+            try {
+                speech.close();
+            } catch (Exception ex) {
+                System.out.println("Problem while closing the speech client");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/com/akruty/pappu/VoiceRecoderService.java
@@ -0,0 +1,69 @@
+package com.akruty.pappu;
+
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javax.sound.sampled.*;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Created by vpati011 on 9/3/17.
+ */
+public class VoiceRecoderService extends Service<byte[]> {
+
+    private int counter = 1;
+
+    private TargetDataLine microphone;
+
+    private ByteArrayOutputStream voiceOutput;
+
+    public VoiceRecoderService() {
+        counter = 1;
+
+        AudioFormat format = new AudioFormat(16000.0f, 16, 1, true, false);
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+
+        if (!AudioSystem.isLineSupported(info)) {
+            System.err.println("Unable to access microphone");
+            System.exit(1);
+        }
+
+        try {
+            microphone = AudioSystem.getTargetDataLine(format);
+            microphone.open(format);
+        } catch (LineUnavailableException e) {
+            System.err.println("Unable to access microphone");
+            System.exit(1);
+        }
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public byte[] getVoiceData() {
+        return voiceOutput.toByteArray();
+    }
+
+    @Override
+    protected Task<byte[]> createTask() {
+        return new Task<byte[]>() {
+
+            @Override
+            protected byte[] call() throws Exception {
+                counter = 0;
+                voiceOutput = new ByteArrayOutputStream();
+                int numBytesRead;
+                byte data[] = new byte[microphone.getBufferSize()/5];
+                microphone.start();
+                while (!isCancelled()) {
+                    numBytesRead = microphone.read(data, 0, data.length);
+                    voiceOutput.write(data, 0, numBytesRead);
+                    counter++;
+                }
+                microphone.stop();
+                return voiceOutput.toByteArray();
+            }
+        };
+    }
+}
